a70947de5a41630676004a4a724db6c59ba1cba2
user <pisabev@gmail.com>
2015-05-15 00:25:17 +0300

    auto

--- lib/src/connection.dart
+++ lib/src/connection.dart
@@ -16,7 +16,7 @@ class Connection {
     _createPool() => _pool = new Pool(_uri,
         minConnections: _min,
         maxConnections: _max,
-        leakDetectionThreshold: new Duration(seconds: 10),
+        leakDetectionThreshold: new Duration(seconds: 20),
         restartIfAllConnectionsLeaked: true
     );
 
