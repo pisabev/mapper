a1f3935f6cae991545edee28cc032e88b82d74f9
Petar Sabev <pisabev@gmail.com>
2016-08-03 16:44:30 +0300

    Auto

--- lib/src/mapper.dart
+++ lib/src/mapper.dart
@@ -228,7 +228,7 @@ abstract class Mapper<E extends Entity, C extends Collection<E>, A extends Appli
 
     _escape(String string) => '"$string"';
 
-    E createObject([dynamic data]) => _markObject(entity(manager, data));
+    E createObject([dynamic data]) => _markObject(entity()..manager = manager..init(data));
 
     C createCollection() => collection();
 
