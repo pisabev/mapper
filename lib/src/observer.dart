2fdbaf7b14db5b75362de4ca9a934fa296b5859b
Petar Sabev <pisabev@gmail.com>
2018-08-26 11:04:49 +0300

    .

--- lib/src/observer.dart
+++ lib/src/observer.dart
@@ -5,18 +5,17 @@ typedef ObserverFunction<E extends Entity> = Null Function(EntityContainer<E>);
 enum MEvent { update, create, delete, change }
 
 class Observer<E extends Entity> {
-  Map<MEvent, List<ObserverFunction>> _hook =
-  new Map<MEvent, List<ObserverFunction>>();
+  Map<MEvent, List<ObserverFunction<E>>> _hook =
+      new Map<MEvent, List<ObserverFunction<E>>>();
 
   Observer();
 
-  void addHook(MEvent scope, ObserverFunction func) {
+  void addHook(MEvent scope, ObserverFunction<E> func) {
     if (_hook[scope] == null) _hook[scope] = new List();
     _hook[scope].add(func);
   }
 
-  Future execHooksAsync(
-      MEvent scope, EntityContainer<E> object) async {
+  Future execHooksAsync(MEvent scope, EntityContainer<E> object) async {
     if (!_hook.containsKey(scope)) return;
     for (var f in _hook[scope]) await f(object);
   }
