5d2af31e9d7de3f97e2394244853e26bc0842222
Petar Sabev <pisabev@gmail.com>
2019-04-11 21:51:12 +0300

    .

--- lib/src/unit.dart
+++ lib/src/unit.dart
@@ -67,11 +67,11 @@ class Unit {
 
   Future _doDeletes() => Future.forEach(_delete, (o) => o._mapper.delete(o));
 
-  Future _doNotifyUpdates() => Future.forEach(_notifyUpdate.values, (v) => v());
+  Future _doNotifyUpdates(Map m) => Future.forEach(m.values, (v) => v());
 
-  Future _doNotifyInserts() => Future.forEach(_notifyInsert.values, (v) => v());
+  Future _doNotifyInserts(Map m) => Future.forEach(m.values, (v) => v());
 
-  Future _doNotifyDeletes() => Future.forEach(_notifyDelete.values, (v) => v());
+  Future _doNotifyDeletes(Map m) => Future.forEach(m.values, (v) => v());
 
   Future _begin() => !_started
       ? _manager._connection.execute('BEGIN').then((_) => _started = true)
@@ -97,12 +97,22 @@ class Unit {
       .then((_) => _resetEntities())
       .catchError((e, s) => _rollback().then((_) => new Future.error(e, s)));
 
-  Future commit() => persist()
-      .then((_) => _commit())
-      .then((_) => _doNotifyDeletes())
-      .then((_) => _doNotifyUpdates())
-      .then((_) => _doNotifyInserts())
-      .then((_) => _resetNotifiers())
-      .catchError((e, s) => (_started ? _rollback() : new Future.value())
-          .then((_) => new Future.error(e, s)));
+  Future commit() {
+    Map notifyDelete;
+    Map notifyInsert;
+    Map notifyUpdate;
+    return persist()
+        .then((_) => _commit())
+        .then((_) {
+          notifyDelete = new Map.from(_notifyDelete);
+          notifyInsert = new Map.from(_notifyInsert);
+          notifyUpdate = new Map.from(_notifyUpdate);
+          _resetNotifiers();
+        })
+        .then((_) => _doNotifyDeletes(notifyDelete))
+        .then((_) => _doNotifyUpdates(notifyUpdate))
+        .then((_) => _doNotifyInserts(notifyInsert))
+        .catchError((e, s) => (_started ? _rollback() : new Future.value())
+            .then((_) => new Future.error(e, s)));
+  }
 }
